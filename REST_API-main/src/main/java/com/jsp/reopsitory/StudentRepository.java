261ca02061b12cb8fb1ee6cf15402af506609d0e
Kunal Shinde <98812386+Shinde101@users.noreply.github.com>
2024-03-06 15:47:36 +0530

    Delete REST_API directory

--- REST_API-main/src/main/java/com/jsp/reopsitory/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jsp.reopsitory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jsp.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
